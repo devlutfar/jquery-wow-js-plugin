eebb0b49c2ee7ef68246b8999d734d08deb8e13e
md.lutfar rahman <96845210+devlutfar@users.noreply.github.com>
2022-04-20 15:08:28 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    //js code
+    
+    //wow js
+    wow = new WOW(
+        {
+        boxClass:     'wow',      // default
+        animateClass: 'animated', // default
+        offset:       0,          // default
+        mobile:       true,       // default
+        live:         true        // default
+      }
+      )
+      wow.init();
+    
+
+
+});
